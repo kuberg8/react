daa4cd76ecc4029dce9ddb58fe9cf00cb1d72ab1
Your Name <kuberg8@gmail.com>
2020-06-13 20:29:46 +0500

    Updates

---
+++ precache-manifest.e83482b3b275a386f8a9575e6ffc5bc0.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9a3b01a279598bfe79e6d6ee5be7694e",
+    "url": "/kuberg8/react/index.html"
+  },
+  {
+    "revision": "fb3180b1f351c88e88c6",
+    "url": "/kuberg8/react/static/css/3.8ce54e51.chunk.css"
+  },
+  {
+    "revision": "6877d4a5869da8d6dc76",
+    "url": "/kuberg8/react/static/css/4.d3852a5f.chunk.css"
+  },
+  {
+    "revision": "10ba15bd80d30329ce2d",
+    "url": "/kuberg8/react/static/css/main.ae19530d.chunk.css"
+  },
+  {
+    "revision": "b5d94a2d932530a18cfd",
+    "url": "/kuberg8/react/static/js/2.32d3d9ca.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/kuberg8/react/static/js/2.32d3d9ca.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fb3180b1f351c88e88c6",
+    "url": "/kuberg8/react/static/js/3.7d8d6d8a.chunk.js"
+  },
+  {
+    "revision": "6877d4a5869da8d6dc76",
+    "url": "/kuberg8/react/static/js/4.0ed68547.chunk.js"
+  },
+  {
+    "revision": "10ba15bd80d30329ce2d",
+    "url": "/kuberg8/react/static/js/main.f4ea336c.chunk.js"
+  },
+  {
+    "revision": "e8c24700aabf05f0d51b",
+    "url": "/kuberg8/react/static/js/runtime-main.feb6a04e.js"
+  },
+  {
+    "revision": "c145c92dcbac53bb174b4cd100aafda2",
+    "url": "/kuberg8/react/static/media/ava.c145c92d.png"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/kuberg8/react/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "48d2f088b7b29c6aaf3ea1514e2f6d31",
+    "url": "/kuberg8/react/static/media/preloader.48d2f088.svg"
+  }
+]);
